ed650cb81d7c89451a1dd48518e383d4b76f97c8
vitor r campos <vitorrc314@gmail.com>
2025-01-15 02:56:01 +0900

    switched to using new ui library and importing timer widget from there. Will create all ui components in there going forward

--- src/App.tsx
+++ src/App.tsx
@@ -1,30 +1,38 @@
 import { CssBaseline, ThemeProvider } from '@mui/material';
 import { theme } from './theme'
-import Timer from './features/timed-sessions/organisms/Timer'
+import { TimerWidget } from 'time-management-ui-lib'
 // import reactLogo from './assets/react.svg'
 // import viteLogo from '/vite.svg'
 import './App.css'
 
+// TODO: hardcoding stages for now, eventually we will allow users to create their own stages
+const sessionStages = [
+  {
+    id: 0,
+    name: 'focused work',
+    duration: 40 * 60 * 1000,
+  },
+  {
+    id: 1,
+    name: 'excercise break',
+    duration: 10 * 60 * 1000,
+  },
+  {
+    id: 2,
+    name: 'cleaning task',
+    duration: 10 * 60 * 1000,
+  },
+]
 
 const App = () => {
-
   return (
     <ThemeProvider theme={theme}>
       <CssBaseline />
       <div className='app'>
         <main className="content">
-          {/* <div>
-        <a href="https://vitejs.dev" target="_blank">
-          <img src={viteLogo} className="logo" alt="Vite logo" />
-        </a>
-        <a href="https://react.dev" target="_blank">
-          <img src={reactLogo} className="logo react" alt="React logo" />
-        </a>
-      </div> */}
-          <Timer />
+          <TimerWidget stages={sessionStages} />
         </main>
       </div>
-
     </ThemeProvider>
   )
 }
